fd56b581f43d7ff94e7f3349d1490c71a5cd2b50
jaya531994 <82469116+jaya531994@users.noreply.github.com>
2025-04-27 19:46:37 +0530

    Update singlylinkedlist.py

--- LinkedList/singlylinkedlist.py
+++ LinkedList/singlylinkedlist.py
@@ -1,17 +1,65 @@
 class Node:
-    def __init__(self, value = None) -> None:
-        self.value = value
+    def __init__(self,value):
+        self.value = value 
         self.next = None
 
-class SLingkedList:
-    def __init__(self) -> None:
+class LinkedList:
+    def __init__(self):
         self.head = None
         self.tail = None
+        self.length = 0
+    
+    def __str__(self):
+        temp_node = self.head
+        result = ""
+        while temp_node is not None:
+            result +=str(temp_node.value)
+            if temp_node.next is not None:
+                result += ' ->'
+            temp_node = temp_node.next
+        return result
 
-SignleLingkedList = SLingkedList()
-node1 = Node(1)
-node2 = Node(2)
+    def append(self,value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        
+        self.length +=1
 
-# SignleLingkedList.head = 
+    def prepand(self,value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        
+        self.length +=1
 
-# Insertion of linked list in memory
+    def insert(self,value,index):
+        new_node = None(value)
+        if index < 0  and index > self.length:
+            return False
+        elif self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            temp_node = self.head
+            for _ in range(index-1):
+                pass
+
+
+
+LL= LinkedList()
+#append node
+LL.append(10)
+LL.append(20)
+LL.prepand(50)
+print(LL)
+
+
